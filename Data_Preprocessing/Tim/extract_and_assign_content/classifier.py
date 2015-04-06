72ff44d00c74bca769c44dd1f4af29a40da56c32
WEI-TING LIN <gsl0318@hotmail.com>
2015-04-05 22:51:52 -0400

    Tim: Upload Latex .tex file and midterm report.

--- Data_Preprocessing/Tim/extract_and_assign_content/classifier.py
+++ Data_Preprocessing/Tim/extract_and_assign_content/classifier.py
@@ -5,6 +5,3 @@ labels = ["Routing", "Power outage", "Packet loss", "Natural disaster",\
 "Application configuration", "Maintenance"]
 
 classfiedThreads = [1, 10, 10, 6, 1, 13, 13, 10, 10, 10, 13, 7, 3, 4, 3, 7, 7, 7, 0, 3, 5, 5, 3, 7, 3, 3, 7, 5, 3, 7]
-
-
-
